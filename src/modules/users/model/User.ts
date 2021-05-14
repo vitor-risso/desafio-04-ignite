8ed1446a537b58008f1e1e392e41d0bc242b73ae
vitor-risso <vitor.risso13@gmail.com>
2021-05-14 18:06:40 -0300

    [MODEL] creating user model

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,25 @@
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuid } from "uuid";
 
 class User {
-  // Complete aqui
+
+  id?: string;
+
+  name: string;
+
+  admin: boolean = false;
+
+  email: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+
 }
 
 export { User };
